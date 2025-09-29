a9ee867536a8af3782fcb28ce40625bb02112285
soywbb111 <soywbb111@gmail.com>
2025-09-29 21:42:18 +0700

    import pandas, load the spreadsheet and clean data

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pandas as pd
 # Rest of the file go here...
+# 52 books best sellers in 25 Sep 2025.
+df = pd.read_csv("data/raw/52books_online_results.csv")
+#print(df.head()) #5 rows
+print(df.shape) #rows, columns
+print(df.info()) #name, data type of columns
+print(df.describe()) #get like max, avg, min
+
+#clean data before running an analysis - practice
+
+#drop duplicate
+df.drop_duplicates(inplace=True)
+
+#renaming columns 
+df.rename(columns={"Name":"Title", "User Rating": "Rating", "Publisher_year":"Publication Year"}, inplace=True)
+
+#covert the data type
+
+
